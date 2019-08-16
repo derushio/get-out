f0270e4574389dd77ecb49d5ebd3b1f7bca3ea67
derushio <narumi18wa@gmail.com>
2019-08-16 22:07:32 +0900

    <master>[new file] add ScoreHistory

---
+++ src/components/graph/ScoreHistory.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+.score-history
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ScoreHistory extends Vue {}
+</script>
+
+<style lang='stylus' scoped>
+@require '~@/assets/styles/entry/_variable.styl';
+
+.score-history {}
+</style>
