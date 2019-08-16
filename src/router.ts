bee43aa3352eca3ac8a4179c458e42f54dd4a1b7
derushio <narumi18wa@gmail.com>
2019-08-16 21:20:18 +0900

    <master>[modified] add routes

--- src/router.ts
+++ src/router.ts
@@ -10,5 +10,20 @@ export default new Router({
             name: 'Home',
             component: () => import('@/views/Home.vue'),
         },
+        {
+            path: '/registration',
+            name: 'Registration',
+            component: () => import('@/views/Registration.vue'),
+        },
+        {
+            path: '/costume',
+            name: 'Costume',
+            component: () => import('@/views/Costume.vue'),
+        },
+        {
+            path: '/quests',
+            name: 'Quests',
+            component: () => import('@/views/Quests.vue'),
+        },
     ],
 });
