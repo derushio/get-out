6c8f78a17dda00f2b64e1ed73bfbecee28d3c4f4
derushio <narumi18wa@gmail.com>
2019-08-16 22:39:29 +0900

    <master>[modified] tab

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,8 +4,18 @@ v-layout#Home(fill-height column)
         v-layout.full-height(column)
             v-flex.red(xs7)
                 //- アバター ＋ レベル情報
-            v-flex.blue(xs5)
-                //- 情報グラフ
+            v-flex(xs5)
+                v-tabs(v-model='tab' background-color='primary' dark grow centered)
+                    v-tabs-slider
+                    v-tab スコア
+                    v-tab クエスト履歴
+
+                v-tabs-items.full-height(v-model='tab' fluid)
+                    v-tab-item.full-height
+                        p hoge
+                    v-tab-item.full-height
+                        p fuga
+
 </template>
 
 <script lang='ts'>
@@ -14,7 +24,9 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
     components: {},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    protected tab = 0;
+}
 </script>
 
 <style lang='stylus'>
