9d9b38372c97e68a96e6ccaf515f1358ee022d99
derushio <narumi18wa@gmail.com>
2019-08-17 11:20:31 +0900

    <master>[modified] change entities

--- src/models/entities/User.ts
+++ src/models/entities/User.ts
@@ -2,10 +2,27 @@ import Costume from '@/models/entities/Costume';
 
 export default interface User {
     name: string;
-    level: number;
+    exp: number;
 }
 
-export function getCostumeByLevel(level: number) {
+export function getLevelByExp(exp: number) {
+    let level = 1;
+    if (1000 < exp) {
+        level = 2;
+    } else if (2000 < exp) {
+        level = 3;
+    } else if (3000 < exp) {
+        level = 4;
+    } else if (4000 < exp) {
+        level = 5;
+    }
+
+    return level;
+}
+
+export function getCostumeByExp(exp: number) {
+    const level = getLevelByExp(exp);
+
     const costume = {
         sunglasses: false,
         mask: false,
