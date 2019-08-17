de426e395f2683d81c77fad02e21a31950c31259
derushio <narumi18wa@gmail.com>
2019-08-18 00:06:07 +0900

    <feature/user-leveling>[modified] init処理変更

--- src/views/Registration.vue
+++ src/views/Registration.vue
@@ -17,58 +17,45 @@ v-layout#Registration(fill-height column)
 
             v-stepper-items.full-height
                 v-stepper-content.full-height(:step='1')
-                    h3.mb-3 あなたの情報を教えてください
-                    v-layout.full-height(column)
-                        v-form
-                            v-row
-                                v-col(cols='12' md='4')
-                                    v-text-field(v-model='name' label='氏名' required='')
-                                v-col(cols='12' md='4')
-                                    v-text-field(v-model='gender' label='性別' required='')
-                                v-col(cols='12' md='4')
-                                    v-text-field(v-model='age' label='年齢' required='')
-                        v-row(justify='end')
-                            v-btn(color='primary' @click='step++') 進む
-                    
-                v-stepper-content.full-height(:step='2')
-                    h3.mb-3 あなたは他人に話しかけられるのが苦手ですか？
-                    v-spacer
+                    p こんにちわ、新しい自分を見つけるお手伝いをさせていただきます
+                    p まずは...
+                    h3.mb-3 あなたについて教えてください
+                    v-form
+                        v-row
+                            v-col(cols='12' md='4')
+                                v-text-field(v-model='name' label='氏名' required='')
+                            v-col(cols='12' md='4')
+                                v-text-field(v-model='gender' label='性別' required='')
+                            v-col(cols='12' md='4')
+                                v-text-field(v-model='age' label='年齢' required='')
+                    v-row(justify='end')
+                        v-btn(color='primary' @click='step++') 進む
 
+                v-stepper-content.full-height(:step='2')
+                    h3.mb-3 あなたは他の人に目を合わせるのが苦手ですか？
                     v-row
                         v-btn(color='error' @click='step--') 戻る
                         v-spacer
-                        //- level1 確定
-                        v-btn(color='warning' @click='initialLevel = 1; step = 5') いいえ  
-                        v-btn(color='primary' @click='step++') はい   
+                        v-btn(color='warning' @click='step++') いいえ
+                        v-btn(color='primary' @click='initialLevel = 1; step=5') はい
 
                 v-stepper-content.full-height(:step='3')
-                    h3.mb-3 あなたは他の人に目を合わせるのが苦手ですか？
-                    v-spacer
-
-                    v-row
-                        v-btn(color='error' @click='step--') 戻る
-                        v-spacer
-                        //- level2 確定
-                        v-btn(color='warning' @click='initialLevel = 2; step=5') いいえ
-                        v-btn(color='primary' @click='step++') はい
-
-                v-stepper-content.full-height(:step='4')
-                    
                     h3.mb-3 あなたは他の人に顔を見せるのが苦手ですか？
-                    v-spacer
-
                     v-row
                         v-btn(color='error' @click='step--') 戻る
                         v-spacer
-                        //- level3 確定
-                        v-btn(color='warning' @click='initialLevel = 3; step++') いいえ  
-                        v-btn(color='primary' @click='step++') はい
-                    
-                
-                v-stepper-content.full-height(:step='5')
-                    h3.mb-3 体験期間をお選びください
-                    v-spacer
+                        v-btn(color='warning' @click='step++') いいえ
+                        v-btn(color='primary' @click='initialLevel = 2; step = 5') はい
+                v-stepper-content.full-height(:step='4')
+                    h3.mb-3 あなたは他人に話しかけられるのが苦手ですか？
+                    v-row
+                        v-btn(color='error' @click='step--') 戻る
+                        v-spacer
+                        v-btn(color='warning' @click='initialLevel = 4; step++') いいえ
+                        v-btn(color='primary' @click='initialLevel = 3; step = 5') はい
 
+                v-stepper-content.full-height(:step='5')
+                    h3.mb-3 目標期間をお選びください
                     v-row(align="center")
                         v-btn(color='error' @click='step--') 戻る
                         v-spacer
@@ -76,17 +63,15 @@ v-layout#Registration(fill-height column)
                         v-spacer
                         v-btn(color='primary' @click='step++') 進む
 
-                
+
                 v-stepper-content.full-height(:step='6')
                     h3.mb-3 お疲れ様です！
-                    v-spacer
-
+                    p 新しいあなたに変身して外を出歩いて見ましょう！
                     v-row
-                        
                         v-btn(text large color='light' @click='step=1') やり直す！
                         v-spacer
-                        v-btn(text large color='warning' @click='createUser') OK
-                                                  
+                        v-btn(text large color='warning' @click='createUser') 変身！
+
 </template>
 
 <script lang='ts'>
@@ -111,7 +96,7 @@ export default class Registration extends Vue {
     protected gender = '';
     protected age = '';
     protected time_range = '';
-    
+
     protected async createUser() {
         await this.$vprogress.circularLoading(async () => {
             const user = {
@@ -121,7 +106,7 @@ export default class Registration extends Vue {
                 time_range: this.time_range,
                 exp: (this.initialLevel-1) * 1000,
             } as User;
-            
+
             console.log(getLevelByExp(user.exp));
 
             await UserApi.putUser(user);
