c09541d80842e4a3a151be2982d6bd6594b9b5f1
Kai Yamada <gpioblink@aol.com>
2019-08-17 10:09:59 +0900

    init scoremeter

---
+++ src/components/graph/ScoreMeter.vue
@@ -0,0 +1,16 @@
+<template lang='pug'>
+.score-meter
+</template>
+
+<script lang='ts'>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ScoreMeter extends Vue {}
+</script>
+
+<style lang='stylus' scoped>
+@require '~@/assets/styles/entry/_variable.styl';
+
+.score-meter {}
+</style>
