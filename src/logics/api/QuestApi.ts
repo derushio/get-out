59ffe80ac02231d0f374914f9f588d32bd809e2c
Hisashi Oshima <hisasu00@icloud.com>
2019-08-17 20:16:33 +0900

    クエストapi# Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch feature/adding-quests# Changes to be committed:#	modified:   src/logics/api/QuestApi.ts#	modified:   src/views/Quests.vue#

--- src/logics/api/QuestApi.ts
+++ src/logics/api/QuestApi.ts
@@ -54,7 +54,7 @@ export default class QuestApi {
             level: 1,
             clearTime: 5423543532,
             // tslint:disable-next-line: max-line-length
-            src: 'https://images.unsplash.com/photo-1484813047368-3a2883981427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+            src: 'https://images.unsplash.com/photo-1497470674350-dad852ead0eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
         },
         {
             id: 5,
@@ -84,9 +84,208 @@ export default class QuestApi {
             level: 4,
             clearTime: 5423543532,
             // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1533483595632-c5f0e57a1936?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 8,
+            title: 'ドン・キホーテに行こう',
+            desc: '変装して、近くのドン・キホーテに行ってみよう！',
+            exp: 300,
+            level: 1,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1484813047368-3a2883981427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 9,
+            title: 'イヤホンを外してドン・キホーテに行こう',
+            desc: 'イヤホンを外して近くのドン・キホーテに行ってみよう！',
+            exp: 1000,
+            level: 2,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1529602266431-5e205a27eb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 10,
+            title: 'マスクを外してドン・キホーテに行こう',
+            desc: 'マスクを外してドン・キホーテに行ってみよう！',
+            exp: 2000,
+            level: 3,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
             src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
         },
-        // TODO:
+        {
+            id: 11,
+            title: 'サングラスを外してドン・キホーテに行こう',
+            desc: 'サングラスを外してドン・キホーテに行ってみよう！',
+            exp: 4000,
+            level: 4,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 12,
+            title: 'マクドナルドに行こう',
+            desc: '変装して、近くのマクドナルドに行ってみよう！',
+            exp: 300,
+            level: 1,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1484813047368-3a2883981427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 13,
+            title: 'イヤホンを外してマクドナルドに行こう',
+            desc: 'イヤホンを外して近くのマクドナルドに行ってみよう！',
+            exp: 1000,
+            level: 2,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1529602266431-5e205a27eb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 14,
+            title: 'マスクを外してマクドナルドに行こう',
+            desc: 'マスクを外してマクドナルドに行ってみよう！',
+            exp: 2000,
+            level: 3,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 15,
+            title: 'サングラスを外してマクドナルドに行こう',
+            desc: 'サングラスを外してマクドナルドに行ってみよう！',
+            exp: 4000,
+            level: 4,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 16,
+            title: 'サーティワンに行こう',
+            desc: '変装して、近くのサーティワンに行ってみよう！',
+            exp: 300,
+            level: 1,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1484813047368-3a2883981427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 17,
+            title: 'イヤホンを外してサーティワンに行こう',
+            desc: 'イヤホンを外して近くのサーティワンに行ってみよう！',
+            exp: 1000,
+            level: 2,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1529602266431-5e205a27eb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 18,
+            title: 'マスクを外してサーティワンに行こう',
+            desc: 'マスクを外してサーティワンに行ってみよう！',
+            exp: 2000,
+            level: 3,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 19,
+            title: 'サングラスを外してサーティワンに行こう',
+            desc: 'サングラスを外してサーティワンに行ってみよう！',
+            exp: 4000,
+            level: 4,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 20,
+            title: '本屋に行こう',
+            desc: '変装して、近くの本屋に行ってみよう！',
+            exp: 300,
+            level: 1,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1484813047368-3a2883981427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 21,
+            title: 'イヤホンを外して本屋に行こう',
+            desc: 'イヤホンを外して近くの本屋に行ってみよう！',
+            exp: 1000,
+            level: 2,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1529602266431-5e205a27eb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 22,
+            title: 'マスクを外して本屋に行こう',
+            desc: 'マスクを外して本屋に行ってみよう！',
+            exp: 2000,
+            level: 3,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 23,
+            title: 'サングラスを外して本屋に行こう',
+            desc: 'サングラスを外して本屋に行ってみよう！',
+            exp: 4000,
+            level: 4,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 24,
+            title: '学校に行こう',
+            desc: '変装して、近くの学校に行ってみよう！',
+            exp: 300,
+            level: 1,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1484813047368-3a2883981427?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 25,
+            title: 'イヤホンを外して学校に行こう',
+            desc: 'イヤホンを外して近くの学校に行ってみよう！',
+            exp: 1000,
+            level: 2,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1529602266431-5e205a27eb68?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 26,
+            title: 'マスクを外して学校に行こう',
+            desc: 'マスクを外して学校に行ってみよう！',
+            exp: 2000,
+            level: 3,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
+        {
+            id: 27,
+            title: 'サングラスを外して学校に行こう',
+            desc: 'サングラスを外して学校に行ってみよう！',
+            exp: 4000,
+            level: 4,
+            clearTime: 5423543532,
+            // tslint:disable-next-line: max-line-length
+            src: 'https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60',
+        },
     ];
     // deprecated
     public static async getAllQuests() {
