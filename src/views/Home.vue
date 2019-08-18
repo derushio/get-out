fb442655445f9c7b72e1510dff46874cbdef807a
derushio <narumi18wa@gmail.com>
2019-08-18 12:13:07 +0900

    <master>[modified] layout fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -98,8 +98,8 @@ export default class Home extends Vue {
 @require '~@/assets/styles/entry/_view.styl';
 
 html
-    // scroll-view: true;
-    static-view: true;
+    scroll-view: true;
+    // static-view: true;
 
 #Home
     .main-pane
